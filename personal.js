08765c1c1ba7853aab799cd4c138f4db3843a54d
Lil Willi <demi.adepitan@gmail.com>
2024-07-01 21:53:21 +0100

    Updated portfolio with animations, transitions, and styles

---
+++ personal.js
@@ -0,0 +1,12 @@
+// var x = document.getElementById("myTopnav");
+// var y = document.getElementById("title");
+// function myFunction() {
+//     if (x.className === "topnav") {
+//         y.style.display = "none";
+//         x.className += " responsive";
+
+//     } else {
+//         y.style.display = "block";
+//         x.className = "topnav";
+//     }
+// }
